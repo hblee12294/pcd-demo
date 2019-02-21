af534541ec95b63d6f7fb7e205d345a911266515
Hongbin Li <hblee12294@gmail.com>
2019-02-21 01:40:53 -0800

    feat: add pcdloader

--- src/Component/View/View.js
+++ src/Component/View/View.js
@@ -1,53 +1,82 @@
 import React, { Component } from 'react'
 
+import 'three/examples/js/loaders/PCDLoader'
 import './View.css'
+import data from './data/Zaghetto.pcd'
 
 class View extends Component {
-  componentDidMount() {
-    const width = this.mount.clientWidth
-    const height = this.mount.clientHeight
-    //ADD SCENE
-    this.scene = new THREE.Scene()
-    //ADD CAMERA
-    this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
-    this.camera.position.z = 4
-    //ADD RENDERER
-    this.renderer = new THREE.WebGLRenderer({ antialias: true })
-    this.renderer.setClearColor('#000000')
-    this.renderer.setSize(width, height)
-    this.mount.appendChild(this.renderer.domElement)
-    //ADD CUBE
-    const geometry = new THREE.BoxGeometry(1, 1, 1)
-    const material = new THREE.MeshBasicMaterial({ color: '#433F81' })
-    this.cube = new THREE.Mesh(geometry, material)
-    this.scene.add(this.cube)
-    this.start()
+  constructor(props) {
+    super(props)
+
+    this.mount
+    this.scene
+    this.camera
+    this.renderer
   }
+
+  componentDidMount() {
+    this.width = this.mount.clientWidth
+    this.height = this.mount.clientHeight
+
+    this.init()
+  }
+
   componentWillUnmount() {
-    this.stop()
+    cancelAnimationFrame(this.frameId)
     this.mount.removeChild(this.renderer.domElement)
   }
-  start = () => {
-    if (!this.frameId) {
-      this.frameId = requestAnimationFrame(this.animate)
-    }
+
+  init = () => {
+    this.buildScene()
+    this.buildCamera()
+    this.buildRenderer()
+    this.buildSubject()
+
+    requestAnimationFrame(this.animate)
   }
-  stop = () => {
-    cancelAnimationFrame(this.frameId)
+
+  buildScene = () => {
+    this.scene = new THREE.Scene()
   }
+
+  buildCamera = () => {
+    this.camera = new THREE.PerspectiveCamera(
+      15,
+      this.width / this.height,
+      0.01,
+      40
+    )
+    this.camera.position.z = 4
+  }
+
+  buildRenderer = () => {
+    this.renderer = new THREE.WebGLRenderer({ antialias: true })
+    this.renderer.setClearColor('#000000')
+    this.renderer.setSize(this.width, this.height)
+    this.mount.appendChild(this.renderer.domElement)
+  }
+
+  buildSubject = () => {
+    let loader = new THREE.PCDLoader()
+    loader.load(data, mesh => {
+      console.log(mesh)
+      this.scene.add(mesh)
+    })
+  }
+
   animate = () => {
-    this.cube.rotation.x += 0.01
-    this.cube.rotation.y += 0.01
     this.renderScene()
-    this.frameId = window.requestAnimationFrame(this.animate)
+    requestAnimationFrame(this.animate)
   }
+
   renderScene = () => {
     this.renderer.render(this.scene, this.camera)
   }
+
   render() {
     return (
       <div
-        style={{ width: '400px', height: '400px' }}
+        style={{ width: '100vw', height: '100vh' }}
         ref={mount => {
           this.mount = mount
         }}
